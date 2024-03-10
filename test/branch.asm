6f0702cfd5663bf855075b8ea35a5b217ca3ae9e
Michael Kohn <mike@mikekohn.net>
2024-03-10 17:22:02 -0500

    Add long backwards test to branch test.

--- test/branch.asm
+++ test/branch.asm
@@ -2,14 +2,25 @@
 
 .org 0x4000
 main:
-  add.w #0x8000, d2
+  add.w #0x4000, d2
   bpl.w blah
 
   move.w #0x80, d0
   trap #0
 
+  move.w #17, d0
+  trap #0
+roar:
+  move.w #7, d0
+  nop
+  nop
+  nop
+  nop
+  trap #1
+
 .org 0x4200
 blah:
   move.w #0x08, d0
+  bra.w roar
   trap #0
 
