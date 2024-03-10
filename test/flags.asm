a72762d4f1107cb6278514bd61291e5cf4d28422
Michael Kohn <mike@mikekohn.net>
2024-03-10 17:22:39 -0500

    Test flags with quick sub.

--- test/flags.asm
+++ test/flags.asm
@@ -2,12 +2,16 @@
 
 .org 0x4000
 main:
-  move.w #0xff00, d0
-  add.w #1, d0
-  ;move ccr, d0
+  ;move.w #0xff00, d0
+  ;add.w #1, d0
 
-  move.w #0xf000, d2
-  move d2, sr
-  move sr, d0 
+  move.w #1, d2
+  subq.w #1, d2
+  ;move.w d2, d0
+  move ccr, d0
+
+  ;move.w #0xf000, d2
+  ;move d2, sr
+  ;move sr, d0
   trap #0
 
